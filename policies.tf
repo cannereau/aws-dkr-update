b1925aa16591a6ea46de078fec40f673d3abab8d
cannereau <c.annereau@fotlan.com>
2025-12-17 16:25:51 +0100

    Fix log group format

--- policies.tf
+++ policies.tf
@@ -41,7 +41,7 @@ data "aws_iam_policy_document" "monitoring" {
     ]
     resources = [
       format(
-        "arn:aws:logs:*:*:log-group:/aws/%s:log-stream:*",
+        "arn:aws:logs:*:*:log-group:/aws/lambda/%s:log-stream:*",
         aws_lambda_function.dkr.function_name
       )
     ]
